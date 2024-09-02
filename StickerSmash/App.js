19cc12dfb1f49d862eed1fdad8530928fcb90ca8
CanOguzz <mustafacanoguz@gmail.com>
2024-09-02 19:44:42 +0300

    Add Button and ImageViewer components

--- StickerSmash/App.js
+++ StickerSmash/App.js
@@ -1,16 +1,24 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Image } from "react-native";
 
-const PlaceholderImage = require("./assets/background-image.png");
+import Button from "./components/Button";
+import ImageViewer from "./components/ImageViewer";
+
+const PlaceholderImage = require("./assets/images/background-image.png");
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <Image source={PlaceholderImage} style={styles.image} />
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
+        
         <Text style={{ color: "#fff" }}>
           Open up App.js to start working on your app!
         </Text>
+        <View style={styles.footerContainer}>
+          <Button label="Choose a photo" />
+          <Button label="Use this photo" />
+        </View>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -20,8 +28,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
+    backgroundColor: "#25292e",
+    alignItems: "center",
   },
   imageContainer: {
     flex: 1,
@@ -31,5 +39,9 @@ const styles = StyleSheet.create({
     width: 320,
     height: 440,
     borderRadius: 18,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
   },
 });
